10a6a138d52ff921a0c3226b9229f368f4ba1886
Carinabl <carinablopez@gmail.com>
2025-08-21 10:15:40 -0300

    se eliminaron archivos

--- frontend/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Reemplazar variables en el JavaScript compilado
-API_URL=${REACT_APP_API_URL:-http://backend:8080}
-find /usr/share/nginx/html -name '*.js' -exec sed -i 's|http://localhost:8080|'"$API_URL"'|g' {} \;
-
-# Iniciar Nginx
-exec nginx -g 'daemon off;'
